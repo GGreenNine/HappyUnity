5a588b8bd2714e208615d5a419447a4d4fa0bcc8
Grigory <uneexpff@gmail.com>
2019-09-16 21:31:23 +0300

    Added HappyMath

---
+++ Math/HappyMath.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HappyUnity.Math
+{
+    public class HappyMath : MonoBehaviour
+    {
+        /// <summary>
+        /// Remaps a value x in interval [A,B], to the proportional value in interval [C,D]
+        /// </summary>
+        /// <param name="x">The value to remap.</param>
+        /// <param name="A">the minimum bound of interval [A,B] that contains the x value</param>
+        /// <param name="B">the maximum bound of interval [A,B] that contains the x value</param>
+        /// <param name="C">the minimum bound of target interval [C,D]</param>
+        /// <param name="D">the maximum bound of target interval [C,D]</param>
+        public static float Remap(float x, float A, float B, float C, float D)
+        {
+            float remappedValue = C + (x-A)/(B-A) * (D - C);
+            return remappedValue;
+        }
+        
+        /// <summary>
+        /// Returns a random success based on X% of chance.
+        /// </summary>
+        /// <param name="percent">Percent of chance.</param>
+        public static bool Chance(int percent)
+        {
+            return (UnityEngine.Random.Range(0,100) <= percent);
+        }
+        
+        /// <summary>
+        /// Projects a point on a line (perpendicularly) and returns the projected point.
+        /// </summary>
+        /// <returns>The point on line.</returns>
+        /// <param name="point">Point.</param>
+        /// <param name="lineStart">Line start.</param>
+        /// <param name="lineEnd">Line end.</param>
+        public static Vector3 ProjectPointOnLine(Vector3 point, Vector3 lineStart, Vector3 lineEnd)
+        {
+            Vector3 rhs = point - lineStart;
+            Vector3 vector2 = lineEnd - lineStart;
+            float magnitude = vector2.magnitude;
+            Vector3 lhs = vector2;
+            if (magnitude > 1E-06f)
+            {
+                lhs = (Vector3)(lhs / magnitude);
+            }
+            float num2 = Mathf.Clamp(Vector3.Dot(lhs, rhs), 0f, magnitude);
+            return (lineStart + ((Vector3)(lhs * num2)));
+        }
+    }  
+}
+
