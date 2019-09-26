2c47a9d3f3ca3eb1c292b87bbafc97fad446d8ad
Grigory <Uneexpff@gmail.cim>
2019-09-26 15:12:14 +0500

    UIUtils added

---
+++ UI/UIUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace HappyUnity.UI
+{
+    public static class UIUtils
+    {
+        public static bool LayerInLayerMask(int layer, LayerMask layerMask)
+        {
+            return ((1 << layer) & layerMask) != 0;
+        }
+    }
+}
