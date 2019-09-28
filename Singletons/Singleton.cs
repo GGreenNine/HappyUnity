e3dc47aff1bf586b61151c593dbfbda1cea94020
Grigory <uneexpff@gmail.com>
2019-09-28 15:31:29 +0300

    Added new Singletons

---
+++ Singletons/Singleton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HappyUnity.Singletons
+{
+    public class Singleton<T> : MonoBehaviour where T : Component
+    {
+        /// <summary>
+        /// Singleton pattern
+        /// </summary>
+        protected static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+                    if (_instance == null)
+                    {
+                        GameObject newInstance = new GameObject();
+                        _instance = newInstance.AddComponent<T>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            if (_instance == null)
+            {
+                _instance = this as T;
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+    }
+
+}
