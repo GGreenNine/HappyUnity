9dfc5953ccd1cb29587ee5a3b508be47bdca0524
Grigory <Uneexpff@gmail.cim>
2019-10-24 14:20:14 +0500

    Replaced extensions to folder "Extensions"

---
+++ Extensions/QuaternionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class QuaternionExtensions
+{
+    public static bool ApproximatelyEqual(this Quaternion q1, Quaternion q2, float precision)
+    {
+        return Mathf.Abs(Quaternion.Dot(q1, q2)) >= 1 - precision;
+    }
+}
---
+++ Extensions/VectorExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class VectorExtensions 
+{
+    public static bool ApproximatelyEqual(this Vector2 current, Vector2 other, float precision)
+    {
+        return Vector2.SqrMagnitude(current - other) < precision;
+    }
+
+    public static bool ApproximatelyEqual(this Vector3 lhs, Vector3 rhs, float precision)
+    {
+        return Vector3.SqrMagnitude(lhs - rhs) < precision;
+    }
+
+    public static bool ApproximatelyEqual(this Vector4 lhs, Vector4 rhs, float precision)
+    {
+        return Vector4.SqrMagnitude(lhs - rhs) < precision;
+    }
+        
+    /// <summary>
+    /// Projects a point on a line (perpendicularly) and returns the projected point.
+    /// </summary>
+    /// <returns>The point on line.</returns>
+    /// <param name="point">Point.</param>
+    /// <param name="lineStart">Line start.</param>
+    /// <param name="lineEnd">Line end.</param>
+    public static Vector3 ProjectPointOnLine(this Vector3 point, Vector3 lineStart, Vector3 lineEnd)
+    {
+        Vector3 rhs = point - lineStart;
+        Vector3 vector2 = lineEnd - lineStart;
+        float magnitude = vector2.magnitude;
+        Vector3 lhs = vector2;
+        if (magnitude > 1E-06f)
+        {
+            lhs = (Vector3)(lhs / magnitude);
+        }
+        float num2 = Mathf.Clamp(Vector3.Dot(lhs, rhs), 0f, magnitude);
+        return (lineStart + ((Vector3)(lhs * num2)));
+    }
+}
+}
