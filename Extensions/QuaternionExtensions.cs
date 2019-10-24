c6c4ff89758d5b72be0dfbf5310b87f61cf3dce3
Grigory <Uneexpff@gmail.cim>
2019-10-24 15:53:47 +0500

    Refactoring Extensions

--- Extensions/QuaternionExtensions.cs
+++ Extensions/QuaternionExtensions.cs
@@ -1,11 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public static class QuaternionExtensions
+namespace HappyUnity.Extensions
 {
-    public static bool ApproximatelyEqual(this Quaternion q1, Quaternion q2, float precision)
+    public static class QuaternionExtensions
     {
-        return Mathf.Abs(Quaternion.Dot(q1, q2)) >= 1 - precision;
+        
+        /// <summary>
+        /// Checks the approximate position of quaternions
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <param name="precision"></param>
+        /// <returns></returns>
+        public static bool ApproximatelyEqual(this Quaternion q1, Quaternion q2, float precision)
+        {
+            return Mathf.Abs(Quaternion.Dot(q1, q2)) >= 1 - precision;
+        }
+
+        public static void SetX(this Quaternion q, float x)
+        {
+            q.x = x;
+        }
+        public static void SetY(this Quaternion q, float y)
+        {
+            q.y = y;
+        }
+        public static void SetZ(this Quaternion q, float z)
+        {
+            q.z = z;
+        }
+        public static void SetW(this Quaternion q, float w)
+        {
+            q.w = w;
+        }
+    
+        public static Quaternion ScalarMultiply(this Quaternion input, float scalar)
+        {
+            return new Quaternion(input.x * scalar, input.y * scalar, input.z * scalar, input.w * scalar);
+        }
+    
+    
     }
 }
--- Extensions/VectorExtensions.cs
+++ Extensions/VectorExtensions.cs
@@ -1,43 +1,75 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public static class VectorExtensions 
+namespace HappyUnity.Extensions
 {
-    public static bool ApproximatelyEqual(this Vector2 current, Vector2 other, float precision)
+    public static class VectorExtensions 
     {
-        return Vector2.SqrMagnitude(current - other) < precision;
-    }
-
-    public static bool ApproximatelyEqual(this Vector3 lhs, Vector3 rhs, float precision)
-    {
-        return Vector3.SqrMagnitude(lhs - rhs) < precision;
-    }
-
-    public static bool ApproximatelyEqual(this Vector4 lhs, Vector4 rhs, float precision)
-    {
-        return Vector4.SqrMagnitude(lhs - rhs) < precision;
-    }
         
-    /// <summary>
-    /// Projects a point on a line (perpendicularly) and returns the projected point.
-    /// </summary>
-    /// <returns>The point on line.</returns>
-    /// <param name="point">Point.</param>
-    /// <param name="lineStart">Line start.</param>
-    /// <param name="lineEnd">Line end.</param>
-    public static Vector3 ProjectPointOnLine(this Vector3 point, Vector3 lineStart, Vector3 lineEnd)
-    {
-        Vector3 rhs = point - lineStart;
-        Vector3 vector2 = lineEnd - lineStart;
-        float magnitude = vector2.magnitude;
-        Vector3 lhs = vector2;
-        if (magnitude > 1E-06f)
+        /// <summary>
+        /// Checks the approximate position of vectors
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <param name="precision"></param>
+        /// <returns></returns>
+        public static bool ApproximatelyEqual(this Vector2 current, Vector2 other, float precision)
         {
-            lhs = (Vector3)(lhs / magnitude);
+            return Vector2.SqrMagnitude(current - other) < precision;
         }
-        float num2 = Mathf.Clamp(Vector3.Dot(lhs, rhs), 0f, magnitude);
-        return (lineStart + ((Vector3)(lhs * num2)));
-    }
+        
+        /// <summary>
+        /// Projects a point on a line (perpendicularly) and returns the projected point.
+        /// </summary>
+        /// <returns>The point on line.</returns>
+        /// <param name="point">Point.</param>
+        /// <param name="lineStart">Line start.</param>
+        /// <param name="lineEnd">Line end.</param>
+        public static Vector3 ProjectPointOnLine(this Vector3 point, Vector3 lineStart, Vector3 lineEnd)
+        {
+            Vector3 rhs = point - lineStart;
+            Vector3 vector2 = lineEnd - lineStart;
+            float magnitude = vector2.magnitude;
+            Vector3 lhs = vector2;
+            if (magnitude > 1E-06f)
+            {
+                lhs = (Vector3)(lhs / magnitude);
+            }
+            float num2 = Mathf.Clamp(Vector3.Dot(lhs, rhs), 0f, magnitude);
+            return (lineStart + ((Vector3)(lhs * num2)));
+        }
+        
+        /// <summary>
+        /// Changes X position
+        /// </summary>
+        /// <param name="current"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static Vector3 SetX(this Vector3 current, float value)
+        {
+            return new Vector3(value, current.y,current.z);
+        }
+        
+        /// <summary>
+        /// Changes Y position
+        /// </summary>
+        /// <param name="current"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static Vector3 SetY(this Vector3 current, float value)
+        {
+            return new Vector3(current.x, value,current.z);
+        }
+        
+        /// <summary>
+        /// Changes Z position
+        /// </summary>
+        /// <param name="current"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static Vector3 SetZ(this Vector3 current, float value)
+        {
+            return new Vector3(current.x, current.y,value);
+        }
+        
 }
 }
