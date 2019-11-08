6c1f0bd14d0112fae624f1016949292d621c2dad
Grigory <Uneexpff@gmail.cim>
2019-11-08 16:08:39 +0500

    Added VR extensions

---
+++ VR/InteractiveObject.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HappyUnity.UI;
+using Stereometry.Scripts.MiniBehaviours;
+
+#if ValveVR
+using Valve.VR.InteractionSystem;
+#endif
+
+
+public class InteractiveObject : MonoBehaviour
+{
+    public LayerMask InteractionLayers;
+    protected Interactable interactable;
+    protected FollowTarget followTarget;
+    [HideInInspector] public bool isSelected;
+
+    protected virtual void Awake()
+    {
+        interactable = GetComponent<Interactable>();
+        if (interactable)
+        {
+#if ValveVR
+            interactable.onAttachedToHand += InteractableOnAttachedToHand;
+            interactable.onDetachedFromHand += InteractableOnDetachedFromHand;
+#endif
+        }
+
+        followTarget = gameObject.GetComponent<FollowTarget>();
+    }
+
+    protected virtual void InteractableOnDetachedFromHand(Hand hand)
+    {
+    }
+
+    protected virtual void InteractableOnAttachedToHand(Hand hand)
+    {
+    }
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        Colliding(other.gameObject, true);
+    }
+
+    private void OnDestroy()
+    {
+#if ValveVR
+        interactable.onAttachedToHand -= InteractableOnAttachedToHand;
+        interactable.onDetachedFromHand -= InteractableOnDetachedFromHand;
+#endif
+    }
+
+
+    protected virtual void OnTriggerExit(Collider other)
+    {
+        Colliding(other.gameObject, false);
+    }
+
+
+    protected virtual void EntryEvent()
+    {
+    }
+
+    protected virtual void ExitEvent()
+    {
+    }
+
+    private void Colliding(GameObject collider, bool enter)
+    {
+        if (!UIUtils.LayerInLayerMask(collider.layer, InteractionLayers))
+            return;
+
+        if (enter)
+            EntryEvent();
+        else
+            ExitEvent();
+    }
+}
+
+public interface ISelectable<out T> where T : InteractiveObject
+{
+    Material RenderingMaterial { get; set; }
+    bool IsSelected { get; set; }
+    T GetInteractable();
+}
---
+++ VR/Selector.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+#if ValveVR
+using Valve.VR.InteractionSystem;
+#endif
+
+namespace Stereometry.Scripts.MiniBehaviours
+{
+    public class Hand
+    {
+        /*
+         * Replace this on Valve Hand script
+         */
+        public Interactable hoveringInteractable;
+    }
+
+    public class Interactable : MonoBehaviour
+    {
+        /*
+         * Replace this on Valve Interactable script
+         */
+    }
+    public class Selector : MonoBehaviour
+    {
+        public Material selectedMaterial;
+
+        public static readonly List<KeyValuePair<ISelectable<InteractiveObject>, Material>> SelectedObjects =
+            new List<KeyValuePair<ISelectable<InteractiveObject>, Material>>();
+
+        private Hand hand;
+        public static InteractiveObject selectedObject = null;
+
+        public Interactable hoveringObject => hand.hoveringInteractable;
+
+        public delegate void ObjectSelectedDelegate(InteractiveObject obj);
+
+        public event ObjectSelectedDelegate On_ObjectSelected;
+
+        private void Awake()
+        {
+            hand = GetComponent<Hand>();
+        }
+
+        private void Start()
+        {
+            InputManager.OnObjectSelectedOn += OnSelected;
+        }
+
+        private void OnDestroy()
+        {
+            InputManager.OnObjectSelectedOn -= OnSelected;
+        }
+
+        /// <summary>
+        /// Unselecting all selected items
+        /// </summary>
+        public static void UnselectAll()
+        {
+            foreach (var selectedObject in SelectedObjects)
+            {
+                selectedObject.Key.RenderingMaterial = selectedObject.Value;
+                selectedObject.Key.IsSelected = false;
+            }
+
+            selectedObject = null;
+            SelectedObjects.Clear();
+        }
+
+        /// <summary>
+        /// Unselecting item
+        /// </summary>
+        /// <param name="selectable_Object"></param>
+        public static void Unselect(InteractiveObject selectable_Object)
+        {
+            selectable_Object.isSelected = false;
+            
+            if (selectedObject == selectable_Object)
+                selectedObject = null;
+            
+            if (!SelectedObjects.Any()) return;
+            
+            var objectToDelete = SelectedObjects.First(x => x.Key == selectable_Object);
+            
+            if (objectToDelete.Key != null)
+                SelectedObjects.Remove(objectToDelete);
+        }
+
+        /// <summary>
+        /// Selecting item
+        /// </summary>
+        /// <param name="selectable_Object"></param>
+        /// <typeparam name="T"></typeparam>
+        private void Select<T>(ISelectable<T> selectable_Object) where T : InteractiveObject
+        {
+            selectedObject = selectable_Object.GetInteractable();
+            
+            if (selectedObject == null)
+                return;
+            if (selectable_Object.IsSelected)
+                return;
+            
+            SelectedObjects.Add(new KeyValuePair<ISelectable<InteractiveObject>, Material>(selectable_Object,
+                selectable_Object.RenderingMaterial));
+            
+            selectable_Object.RenderingMaterial = selectedMaterial;
+            selectable_Object.IsSelected = true;
+        }
+
+        /// <summary>
+        /// Called by event
+        /// </summary>
+        private void OnSelected()
+        {
+            if (hoveringObject == null)
+            {
+                UnselectAll();
+                return;
+            }
+
+            if (!(hoveringObject.GetComponent<InteractiveObject>() is ISelectable<InteractiveObject>
+                ISelectable))
+                return;
+
+            Select(ISelectable);
+
+            On_ObjectSelected?.Invoke(selectedObject);
+        }
+    }
+}
